b96d2873501b231c0cdf0348ff0442736677a083
kisenge <76875002+kisenge@users.noreply.github.com>
2025-02-04 05:43:07 -0400

    django app create + setup

--- src/urlScannerApi/scannerApiProject/settings.py
+++ src/urlScannerApi/scannerApiProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scannerApiApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
